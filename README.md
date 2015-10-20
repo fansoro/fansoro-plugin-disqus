261cc1b6f3993fa63bd93104913adaa9b35eb8f7
Awilum <awilum@msn.com>
2015-10-20 23:14:48 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# morfy-plugin-disqus
-Disqus plugin for Morfy
+# Morfy Disqus Plugin
+Morfy disqus plugin.
+
+Use this code in Morfy Templates to show Disqus comments:
+```
+{Disqus::comments()}
+```
+
+## License
+See [LICENSE](https://github.com/morfy-cms/morfy-plugin-diqus/blob/master/LICENSE)
---
+++ disqus.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Morfy Disqus Plugin
+ *
+ * (c) Romanenko Sergey / Awilum <http://morfy.org>
+ * (c) Moncho Varela / Nakome <http://monchovarela.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Disqus
+{
+    public static function comments()
+    {
+        $html = '<div id="disqus_thread"></div>
+    		<script type="text/javascript">
+    			var disqus_shortname = "'.Monstra::$plugins['disqus']['disqus_shortname'].'";
+    			(function() {
+    				var dsq = document.createElement("script");
+    				dsq.type = "text/javascript";
+    				dsq.async = true;
+    				dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+    				(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
+    			})();
+    		</script>
+    		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    		<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a> ';
+        echo $html;
+    }
+}
